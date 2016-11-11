020eb81e0a7cacd31cf368ef71f1119e7a99ec10
Andre Abreu <andre.abreu@ubs.com>
2016-11-11 17:12:23 +0000

    Added field and state machine logic to the parser

--- parse.c
+++ parse.c
@@ -1,5 +1,6 @@
 /* standard includes */
 #include <stdio.h>
+#include <string.h>
 
 /* application includes */
 #include "parse.h"
@@ -7,12 +8,30 @@
 /* json-c (https://github.com/json-c/json-c) */
 #include <json.h>
 
+/* forward Declaration */
+void stateUpdate(char *key);
+
+typedef enum {
+	TYPE,
+	ID, NAME,
+  	STATUSSEVERITY, STATUSSEVERITYDESCRIPTION, REASON,
+    SKIP
+} parse_field;
+
+/* the current field of the parser */
+parse_field field = SKIP;
+
+/* state flags */
+typedef enum { false, true } bool;
+bool isInLine = false;
+bool isInLineStatuses = false;
+
 /* printing the value corresponding to boolean, double, integer and strings */
 void print_json_value(struct json_object *jobj) {
- 	printf(">>> print_json_value\n"); 
+ 	//printf(">>> print_json_value\n"); 
 
 	enum json_type type = json_object_get_type(jobj); /* getting the type of the json object */
-	printf("type: %u\n", type);
+	//printf("type: %u\n", type);
   	switch (type) {
  		case json_type_null:
 		case json_type_object:
@@ -20,25 +39,51 @@ void print_json_value(struct json_object *jobj) {
      		break;
 
     	case json_type_boolean:
-			printf("value: %s\n", json_object_get_boolean(jobj)? "true": "false");
+			//printf("value: %s\n", json_object_get_boolean(jobj)? "true": "false");
             break;
 
     	case json_type_double:
-        	printf("value: %lf\n", json_object_get_double(jobj));
+        	//printf("value: %lf\n", json_object_get_double(jobj));
             break;
 
     	case json_type_int:
-        	printf("value: %d\n", json_object_get_int(jobj));
-            break;
+		{
+			int value = json_object_get_int(jobj);
+        	//printf("value: %d\n", value); 
+       		if (field == STATUSSEVERITY) { 
+				printf("StatusSeverity: %d\n", value);
+			} 	
+		}
+		   break;
 
     	case json_type_string:
-        	printf("value: %s\n", json_object_get_string(jobj));
-            break;
+		{
+			const char *value = json_object_get_string(jobj);
+        	//printf("value: %s\n", value); 
+
+			if (field == TYPE) {
+ 				if (strncmp("Tfl.Api.Presentation.Entities.Line, Tfl.Api.Presentation.Entities", value, strlen(value)) == 0) {
+					isInLine = true;
+					isInLineStatuses = false;
+					printf("--------------------\n");
+ 				}			
+			
+			} else if (field == ID) {
+				printf("Id: %s\n", value); 
+			} else if (field == NAME) {
+				printf("Name: %s\n", value); 
+			} else if (field == STATUSSEVERITYDESCRIPTION) {
+				printf("StatusSeverityDescription: %s\n", value); 
+			} else if (field == REASON) {
+				printf("Reason: %s\n", value);
+			}
+		} 
+			break;
 	}
 }
 
 void json_parse_array(struct json_object *jobj, char *key) {
- 	printf(">>> json_parse_array\n"); 
+ 	//printf(">>> json_parse_array\n"); 
 
 	void json_parse(struct json_object *jobj); /* forward Declaration */
 	enum json_type type;
@@ -52,7 +97,7 @@ void json_parse_array(struct json_object *jobj, char *key) {
 	}	
 
   	int arraylen = json_object_array_length(jarray); /* getting the length of the array */
-  	printf("Array Length: %d\n", arraylen);
+  	//printf("Array Length: %d\n", arraylen);
   
 	int i;
   	struct json_object *jvalue;
@@ -60,13 +105,12 @@ void json_parse_array(struct json_object *jobj, char *key) {
   	for (i=0; i< arraylen; i++) {
     	jvalue = json_object_array_get_idx(jarray, i); /* getting the array element at position i */
     	type = json_object_get_type(jvalue);
-		printf("type: %u\n", type);
 
     	if (type == json_type_array) {
     	  	json_parse_array(jvalue, NULL);
 
     	} else if (type != json_type_object) {
-	     	printf("value[%d]: ",i);
+	     	//printf("value[%d]: ", i);
     	  	print_json_value(jvalue);
     	
 		} else {
@@ -77,10 +121,10 @@ void json_parse_array(struct json_object *jobj, char *key) {
 
 /* parsing the json object */
 void json_parse(struct json_object *jobj) {
-	printf(">>> json_parse\n");	
+	//printf(">>> json_parse\n");	
 
 	enum json_type type = json_object_get_type(jobj); /* getting the type of the json object */
-    printf("type: %u\n", type);
+    //printf("type: %u\n", type);
 
 	if (type == json_type_array) {
 		json_parse_array(jobj, NULL);
@@ -91,10 +135,11 @@ void json_parse(struct json_object *jobj) {
 	}
 
 	json_object_object_foreach(jobj, key, val) { /* passing through every array element */
-		printf("Key is [%s] %d", key, (val == NULL));
+		//printf("Key is [%s] %d\n", key, (val == NULL));
+
+		stateUpdate(key);
 
 		type = json_object_get_type(val);
-		printf("type: %u\n", type);
     	switch (type) {
 			case json_type_null:
 				break;
@@ -121,3 +166,34 @@ void json_parse(struct json_object *jobj) {
     	}
 	}	
 }
+
+/* state machine */
+void stateUpdate(char *key) {
+	field = SKIP;
+
+	if (strncmp("$type", key, strlen(key)) == 0) {
+		field = TYPE;
+		return;
+	}
+
+	if (isInLine == true && isInLineStatuses == false) {
+		if (strncmp("id", key, strlen(key)) == 0) {
+			field = ID;	
+		} else if (strncmp("name", key, strlen(key)) == 0) {
+			field = NAME;
+		} else if (strncmp("lineStatuses", key, strlen(key)) == 0) {
+			isInLineStatuses = true;
+			return;	
+		}
+	
+	} else if (isInLine == true && isInLineStatuses == true) { 
+		if (strncmp("statusSeverity", key, strlen(key)) == 0) {
+             field = STATUSSEVERITY;
+         } else if (strncmp("statusSeverityDescription", key, strlen(key)) == 0) {
+             field = STATUSSEVERITYDESCRIPTION;
+         } else if (strncmp("reason", key, strlen(key)) == 0) {
+             field = REASON;
+       }	
+	}
+}
+
